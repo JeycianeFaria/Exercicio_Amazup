03f6fe02e2bc36fc96e92b59d47f32d144e7c975
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-13 14:26:55 -0300

    Criar entidade livro

---
+++ src/main/java/br/com/zup/Amazup/livro/Livro.java
@@ -0,0 +1,70 @@
+package br.com.zup.Amazup.livro;
+
+import br.com.zup.Amazup.autor.Autor;
+import br.com.zup.Amazup.livro.enuns.Genero;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private Autor autor;
+    @Column(nullable = false)
+    private double preco;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Genero genero;
+
+
+    public Livro() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+}
