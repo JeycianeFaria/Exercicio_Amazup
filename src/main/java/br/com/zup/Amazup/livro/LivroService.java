6e71da74e05f426582036e3bd4feb1e5e541b0c5
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-13 14:37:00 -0300

    Criar service para livro

---
+++ src/main/java/br/com/zup/Amazup/livro/LivroService.java
@@ -0,0 +1,12 @@
+package br.com.zup.Amazup.livro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+}
