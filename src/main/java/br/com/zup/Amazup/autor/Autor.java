dd67db78ecde89a11c4a1cf6ea472edb3175937f
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-13 14:25:42 -0300

    Criar entidade Autor

---
+++ src/main/java/br/com/zup/Amazup/autor/Autor.java
@@ -0,0 +1,36 @@
+package br.com.zup.Amazup.autor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String nome;
+
+
+    public Autor() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
