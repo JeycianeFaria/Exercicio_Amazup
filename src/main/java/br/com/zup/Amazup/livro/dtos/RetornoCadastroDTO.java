cdeae88e1128a22ad7fb4ded034201394abacb0a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-13 14:46:32 -0300

    Criar dto para retorno do cadastro do livro

---
+++ src/main/java/br/com/zup/Amazup/livro/dtos/RetornoCadastroDTO.java
@@ -0,0 +1,20 @@
+package br.com.zup.Amazup.livro.dtos;
+
+public class RetornoCadastroDTO {
+
+    private String vitrine;
+
+
+    public RetornoCadastroDTO() {
+    }
+
+
+    public String getVitrine() {
+        return vitrine;
+    }
+
+    public void setVitrine(String vitrine) {
+        this.vitrine = vitrine;
+    }
+
+}
