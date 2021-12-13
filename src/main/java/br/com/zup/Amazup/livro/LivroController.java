62636965f074969eab196d47cb8e1ee912c6e66f
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-13 14:37:31 -0300

    Criar controller para livro

---
+++ src/main/java/br/com/zup/Amazup/livro/LivroController.java
@@ -0,0 +1,17 @@
+package br.com.zup.Amazup.livro;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+
+    @Autowired
+    private LivroService livroService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+}
