01d57701921f40f3ecbd4c190cc28bdf7f204c61
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-14 13:19:38 -0300

    Adicionar método cadastrar livro

--- src/main/java/br/com/zup/Amazup/livro/LivroController.java
+++ src/main/java/br/com/zup/Amazup/livro/LivroController.java
@@ -1,9 +1,13 @@
 package br.com.zup.Amazup.livro;
 
+import br.com.zup.Amazup.livro.dtos.CadastroLivroDTO;
+import br.com.zup.Amazup.livro.dtos.RetornoCadastroDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/livros")
@@ -13,5 +17,18 @@ public class LivroController {
     private LivroService livroService;
     @Autowired
     private ModelMapper modelMapper;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RetornoCadastroDTO cadastrarLivro(@RequestBody @Valid CadastroLivroDTO livroRecebido){
+        Livro livro = livroService.salvarLivro(modelMapper.map(livroRecebido,Livro.class));
+        String vitrine = "http://localhost:8080/livros/" + livro.getId();
+
+        RetornoCadastroDTO retornoCadastroDTO = new RetornoCadastroDTO();
+        retornoCadastroDTO.setVitrine(vitrine);
+
+        return retornoCadastroDTO;
+    }
 
 }
