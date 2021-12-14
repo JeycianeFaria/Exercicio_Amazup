4396c557d67932b5b58ea2373600deb804b57057
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-14 13:21:01 -0300

    Adicionar cenário para testes da service livro

--- src/test/java/br/com/zup/Amazup/livro/LivroServiceTest.java
+++ src/test/java/br/com/zup/Amazup/livro/LivroServiceTest.java
@@ -1,9 +1,16 @@
 package br.com.zup.Amazup.livro;
 
+import br.com.zup.Amazup.autor.Autor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static br.com.zup.Amazup.livro.enuns.Genero.FICCAO_CIENTIFICA;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class LivroServiceTest {
@@ -15,5 +22,22 @@ public class LivroServiceTest {
 
     @Autowired
     private LivroService livroService;
+
+    private Livro livro;
+    private Autor autor;
+
+    @BeforeEach
+    public void  setup(){
+        autor = new Autor();
+        autor.setId(1);
+        autor.setNome("Douglas Adams");
+
+        livro = new Livro();
+        livro.setId(1);
+        livro.setNome("Adeus e Obrigado Pelos Peixes");
+        livro.setAutor(autor);
+        livro.setPreco(39.90);
+        livro.setGenero(FICCAO_CIENTIFICA);
+    }
 
 }
