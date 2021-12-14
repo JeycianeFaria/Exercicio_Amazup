f6ef654bca96b27ec8b0235e2b506a4feed47037
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-14 13:27:38 -0300

    Criar classe mensagem erro

---
+++ src/main/java/br/com/zup/Amazup/config/MensagemErro.java
@@ -0,0 +1,20 @@
+package br.com.zup.Amazup.config;
+
+public class MensagemErro {
+
+    private String mensagem;
+
+    public MensagemErro(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+}
