f12656c77021c3e09834091f1110ccdbbb685944
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-14 15:59:36 -0300

    Adicionar método para buscar livro por id

--- src/main/java/br/com/zup/Amazup/livro/LivroService.java
+++ src/main/java/br/com/zup/Amazup/livro/LivroService.java
@@ -3,6 +3,8 @@ package br.com.zup.Amazup.livro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class LivroService {
 
@@ -14,4 +16,12 @@ public class LivroService {
         return livroRepository.save(livro);
     }
 
+    public Livro buscarLivroPorId(int id) {
+        Optional<Livro> livroOptional = livroRepository.findById(id);
+        if (livroOptional.isEmpty()){
+            throw new RuntimeException();
+        }
+
+        return livroOptional.get();
+    }
 }
