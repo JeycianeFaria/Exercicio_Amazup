0efba0917848ee9d78316450868b9fa59a22e37e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-14 13:26:32 -0300

    Adicionar método teste cadastrar livro

--- src/test/java/br/com/zup/Amazup/livro/LivroControllerTest.java
+++ src/test/java/br/com/zup/Amazup/livro/LivroControllerTest.java
@@ -3,12 +3,19 @@ package br.com.zup.Amazup.livro;
 import br.com.zup.Amazup.autor.Autor;
 import br.com.zup.Amazup.livro.componentes.ConversorModelMapper;
 import br.com.zup.Amazup.livro.dtos.CadastroLivroDTO;
+import br.com.zup.Amazup.livro.dtos.RetornoCadastroDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static br.com.zup.Amazup.livro.enuns.Genero.FICCAO_CIENTIFICA;
 
@@ -47,6 +54,23 @@ public class LivroControllerTest {
         livroCadastro.setAutor(autor);
         livroCadastro.setPreco(39.90);
         livroCadastro.setGenero(FICCAO_CIENTIFICA);
+
+    }
+
+    @Test
+    public void testarCadastrarLivro() throws Exception {
+        Mockito.when(livroService.salvarLivro(Mockito.any(Livro.class))).thenReturn(livro);
+        String json = objectMapper.writeValueAsString(livroCadastro);
+
+        ResultActions respostaRequisicao = mockMvc.perform(MockMvcRequestBuilders.post("/livros")
+                        .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.vitrine")
+                        .value("http://localhost:8080/livros/" + livro.getId())
+                );
+
+        String jsonResposta = respostaRequisicao.andReturn().getResponse().getContentAsString();
+        RetornoCadastroDTO retornoCadastroDTO = objectMapper.readValue(jsonResposta,RetornoCadastroDTO.class);
 
     }
 
