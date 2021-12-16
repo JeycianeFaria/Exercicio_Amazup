1ebd14359502b57bec298f2581cf8b89a8e531d8
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-16 14:30:28 -0300

    Criar dto para cadastro autor

---
+++ src/main/java/br/com/zup/Amazup/autor/dtos/AutorCadastroDto.java
@@ -0,0 +1,20 @@
+package br.com.zup.Amazup.autor.dtos;
+
+public class AutorCadastroDto {
+
+    private String nome;
+
+
+    public AutorCadastroDto() {
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
