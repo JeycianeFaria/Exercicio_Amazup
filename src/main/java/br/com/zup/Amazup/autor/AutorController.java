9b534b0cabfa928b58829176c5526cbe8a29549e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-16 14:28:58 -0300

    Adicionar método para cadastrar autor

--- src/main/java/br/com/zup/Amazup/autor/AutorController.java
+++ src/main/java/br/com/zup/Amazup/autor/AutorController.java
@@ -1,8 +1,11 @@
 package br.com.zup.Amazup.autor;
 
+import br.com.zup.Amazup.autor.dtos.AutorCadastroDto;
+import br.com.zup.Amazup.autor.dtos.AutorUriDto;
 import br.com.zup.Amazup.livro.componentes.ConstrutorURI;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,5 +18,17 @@ public class AutorController {
     private ModelMapper modelMapper;
     @Autowired
     private ConstrutorURI uri;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AutorUriDto cadastrarAutor(@RequestBody AutorCadastroDto autorDto) {
+        Autor autor = autorService.salvarAutor(modelMapper.map(autorDto, Autor.class));
+
+        AutorUriDto autorUriDto = modelMapper.map(autor, AutorUriDto.class);
+        autorUriDto.setUri(uri.criarUri("/autores" + autor.getId()));
+
+        return  autorUriDto;
+    }
 
 }
